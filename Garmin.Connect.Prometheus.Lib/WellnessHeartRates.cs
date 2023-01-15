08f1cf6dd2047fe39257d230053a8db330fbb8d7
Markus Rothlübbers <markus@mrlb.space>
2023-01-15 23:35:07 +0100

    Gets logger in WellnessHeartRates.cs Catches error on login

--- Garmin.Connect.Prometheus.Lib/WellnessHeartRates.cs
+++ Garmin.Connect.Prometheus.Lib/WellnessHeartRates.cs
@@ -1,4 +1,6 @@
 ﻿using System.Globalization;
+using Garmin.Connect.Models;
+using Microsoft.Extensions.Logging;
 using Prometheus;
 
 namespace Garmin.Connect.Prometheus.Lib;
@@ -7,6 +9,7 @@ public class WellnessHeartRates : IWellnessHeartRates
 {
     private const string METRIC_NAME = "wellnessheartrates";
     private readonly IGarminConnectClient _garminConnectClient;
+    private readonly ILogger<WellnessHeartRates> _logger;
 
     private static readonly Gauge RestingHeartRate =
         Metrics.CreateGauge($"{MetricDefaults.METRIC_PREFIX}_{METRIC_NAME}_restingheartrate", "Resting heart rate for the day",
@@ -18,13 +21,24 @@ public class WellnessHeartRates : IWellnessHeartRates
     private static readonly Gauge MaxHeartRate = Metrics.CreateGauge($"{MetricDefaults.METRIC_PREFIX}_{METRIC_NAME}_maxheartrate",
         "Maximum heart rate for the day", "export_date");
 
-    public WellnessHeartRates(IGarminConnectClient garminConnectClient)
+    public WellnessHeartRates(IGarminConnectClient garminConnectClient,
+        ILogger<WellnessHeartRates> logger)
     {
         _garminConnectClient = garminConnectClient;
+        _logger = logger;
     }
     public async Task Export()
     {
-        var heartRates = await _garminConnectClient.GetWellnessHeartRates(DateTime.Today.AddDays(-1));
+        GarminHr heartRates;
+        try
+        {
+            heartRates = await _garminConnectClient.GetWellnessHeartRates(DateTime.Today.AddDays(-1));
+        }
+        catch (Exception e)
+        {
+            _logger.LogError("Garmin Login Error: {Exception}", e);
+            return;
+        }
         var exportDate = heartRates.CalendarDate.ToString(CultureInfo.CurrentCulture);
         RestingHeartRate.WithLabels(exportDate)
             .Set(heartRates.RestingHeartRate);
