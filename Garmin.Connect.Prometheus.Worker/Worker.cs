bc28c0a8e1e67e2c36bd8d397e7ce6c9c730502e
Markus Rothlübbers <markus@mrlb.space>
2023-01-15 01:46:23 +0100

    adds poc for reflection based metric publishing

--- Garmin.Connect.Prometheus.Worker/Worker.cs
+++ Garmin.Connect.Prometheus.Worker/Worker.cs
@@ -7,24 +7,13 @@ public class Worker : BackgroundService
 {
     private static readonly Gauge RestingHeartRate =
         Metrics.CreateGauge("garmin_connect_wellnessheartrates_restingheartrate", "Resting heart rate for the day",
-            labelNames: new []
-            {
-                "export_date"
-            });
+            "export_date");
 
     private static readonly Gauge MinHeartRate = Metrics.CreateGauge("garmin_connect_wellnessheartrates_minheartrate",
-        "Minimun heart rate for the day",
-        labelNames: new []
-        {
-            "export_date"
-        });
+        "Minimun heart rate for the day", "export_date");
 
     private static readonly Gauge MaxHeartRate = Metrics.CreateGauge("garmin_connect_wellnessheartrates_maxheartrate",
-        "Maximum heart rate for the day",
-        labelNames: new []
-        {
-            "export_date"
-        });
+        "Maximum heart rate for the day", "export_date");
 
     private readonly TimeSpan _delay = new(1, 0, 0, 0);
     private readonly IGarminConnectClient _garminConnectClient;
@@ -51,6 +40,11 @@ public class Worker : BackgroundService
 
     private async Task Run(CancellationToken stoppingToken)
     {
+        await ExportWellnessHeartRates();
+    }
+
+    private async Task ExportWellnessHeartRates()
+    {
         var heartRates = await _garminConnectClient.GetWellnessHeartRates(DateTime.Today.AddDays(-1));
         RestingHeartRate.WithLabels(heartRates.CalendarDate.ToString(CultureInfo.CurrentCulture))
             .Set(heartRates.RestingHeartRate);
@@ -58,5 +52,27 @@ public class Worker : BackgroundService
             .Set(heartRates.MinHeartRate);
         MaxHeartRate.WithLabels(heartRates.CalendarDate.ToString(CultureInfo.CurrentCulture))
             .Set(heartRates.MaxHeartRate);
+    }
+
+    /// <summary>
+    /// </summary>
+    private async Task PublishUserSummary()
+    {
+        var userSummary = await _garminConnectClient.GetUserSummary(DateTime.Today.AddDays(-1));
+
+        var gauges = userSummary.GetType()
+            .GetProperties()
+            .Select(propertyInfo => Metrics.CreateGauge(propertyInfo.Name, propertyInfo.Name)).ToList();
+
+        foreach (var gauge in gauges)
+        {
+            var property = userSummary.GetType()
+                .GetProperties()
+                .FirstOrDefault(o => o.Name == gauge.Name);
+            if (double.TryParse(property?.GetValue(userSummary)?.ToString(), out var value))
+            {
+                gauge.Set(value);
+            }
+        }
     }
 }
